3af34793cbe0f8d0a33f93adfffa1d44ce87f4fe
Omar ELGHAFFOULI <omar.elghaffouli@centrale-casablanca.ma>
2021-07-10 17:16:00 +0100

    newww

--- Creating account 2/src/main/java/com/exchange/config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.exchange.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
